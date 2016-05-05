9e0dc0f708b29149253e14b0bdfb17256e3ddb0c
Jacob Hefley <jacobhefley3@gmail.com>
2016-05-05 11:52:22 -0700

    start tests

---
+++ src/test/java/WordTest.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class WordTest {
+
+  @Test
+  public void Word_testConstructor() {
+    Word yourApp = new Word("test");
+    String expected = "word";
+    assertThat(yourApp);
+  }
+
+  @Test
+  public void Word_testConstructor() {
+    Word yourApp = new Word("test");
+    String expected = "word";
+    assertThat(yourApp);
+  }  
+}
